dea12e2ea4f83a46193b3b314d32280da033c5ee
shashankpritam <shashankpritam@outlook.com>
2021-12-15 19:40:48 +0530

    15122021

--- parser.cpp
+++ parser.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+
 using namespace std;
 
 int main ()
 {
-  int i;
+  std::ofstream ofs;
+  string pdb;
   cout << "Please enter the four letter PDB Code here: ";
   cin >> pdb;
-  cout << "The PDB ID you have entered is " << pdb;
-
+  cout << "The PDB ID you have entered is " << pdb << endl;
+  ofs.open("listfile.txt", std::ofstream::out | std::ofstream::trunc);
+  ofs << pdb;
+  ofs.close();
+  string file;
+  string add(".pdb.gz");
+  file = pdb+add;
+  cout << file << endl;
+  //system("/mnt/d/git/c0d3z4pUblik/batch_download.sh; ./batch_download.sh -f listfile.txt -p &");
+  system("gzip -d file.gz");
   return 0;
 }
