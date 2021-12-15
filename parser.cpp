8e473d1a617df733c5b8c99e6fb9fc718fb11cc0
shashankpritam <shashankpritam@outlook.com>
2021-12-15 18:28:10 +0530

    Update parser.cpp

---
+++ parser.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int i;
+  cout << "Please enter the four letter PDB Code here: ";
+  cin >> pdb;
+  cout << "The PDB ID you have entered is " << pdb;
+
+  return 0;
+}
