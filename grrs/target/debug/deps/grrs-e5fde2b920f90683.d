629f3a6fdd584484805d8dcb156abd30af34ec1e
shashankpritam <shashankpritam@outlook.com>
2022-04-24 02:18:12 +0530

    grrs

---
+++ grrs/target/debug/deps/grrs-e5fde2b920f90683.d
@@ -0,0 +1,5 @@
+/mnt/d/git/c0d3z4pUblik/grrs/target/debug/deps/grrs-e5fde2b920f90683: src/main.rs
+
+/mnt/d/git/c0d3z4pUblik/grrs/target/debug/deps/grrs-e5fde2b920f90683.d: src/main.rs
+
+src/main.rs:
---
+++ grrs/target/debug/grrs.d
@@ -0,0 +1,1 @@
+/mnt/d/git/c0d3z4pUblik/grrs/target/debug/grrs: /mnt/d/git/c0d3z4pUblik/grrs/src/main.rs
